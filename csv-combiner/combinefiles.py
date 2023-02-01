49c96116bff591355662718c1863b3a5352a9409
Gianna Jarmain <laxchamp76@icloud.com>
2023-01-31 21:39:58 -0500

    Update

--- csv-combiner/combinefiles.py
+++ csv-combiner/combinefiles.py
@@ -1,39 +1,26 @@
+#Gianna Jarmain
+
 import csv
 import sys
 import os.path as path
 import pandas as pd
 
-readAccessories = pd.read_csv('/Users/giannajarmain/Desktop/ProgrammingChallenges/csv-combiner/fixtures/accessories.csv')
-readClothing = pd.read_csv('/Users/giannajarmain/Desktop/ProgrammingChallenges/csv-combiner/fixtures/clothing.csv')
-readHouseholdItems = pd.read_csv('/Users/giannajarmain/Desktop/ProgrammingChallenges/csv-combiner/fixtures/household_cleaners.csv')
-#outputFile = ('newCombined.csv')
 
-def write_to_outFile(writer, inputFile):
+def write_to_stdout(inputFile):
     fileContents = pd.read_csv(inputFile)
     for i, row in fileContents.iterrows():
-         writer.writerow([
-               row['email_hash'],
-               row['category'],
-               path.basename(inputFile).split('/')[-1]
-         ])
-
          sys.stdout.write(row['email_hash'] + '\t' + row['category'] + '\t' + path.basename(inputFile).split('/')[-1] + '\n')
 
 
 def main():
     arguments = sys.argv
     
-    with open('combined.csv', 'w', encoding='utf-8') as outputFile:
-        writer = csv.writer(outputFile, doublequote=False, escapechar='\\', quoting=csv.QUOTE_ALL)
-        writer.writerow(['email_hash', 'category', 'filename'])
+    
+    sys.stdout.write('email_hash' + '\t' +'category' +'\t' +'filename')
+    sys.stdout.write('\n')
 
-        #Write to stdout
-        sys.stdout.write('email_hash' + '\t' +'category' +'\t' +'filename')
-        sys.stdout.write('\n')
-
-        #We don't want the python script, just the input files. Write to output file.
-        for inputFile in arguments[1:]:
-            write_to_outFile(writer, inputFile)
+    for inputFile in arguments[1:]:
+        write_to_stdout(inputFile)
 
 
 if __name__ == '__main__':
